618fab4e43cd6ef977508b50d9ecb32949ab040e
EddieValiant <ryanlhill@gmail.com>
2015-12-31 01:39:37 -0600

    Added unimplemented button for light control Added connections to openweatherapi Added weather response data contract Added connection to reddit to retrieve news stories Changed css to a dark theme

--- RHAA/Controllers/HomeController.cs
+++ RHAA/Controllers/HomeController.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using System.Net;
+using System.IO;
 using System.Collections.Generic;
-using System.Linq;
+using System.Linq; 
 using System.Web;
 using System.Web.Mvc;
+using RedditSharp;
+using RHAA.Services;
+using RHAA.Models;
+using System.Runtime.Serialization.Json;
 
 namespace RHAA.Controllers
 {
@@ -10,12 +16,53 @@ namespace RHAA.Controllers
     {
         public ActionResult Index()
         {
+            var reddit = new Reddit();
+            //var user = reddit.LogIn("EddieValiantsRabbit", "camaro");
+            var all = reddit.GetSubreddit("/r/news");
+
+            var listOfPosts = all.Hot.Take(25);
+
+            ViewBag.Posts = new List<RedditSharp.Things.Post>();
+            ViewBag.Posts = listOfPosts;
+           
+            ViewBag.Greeting = "Good " + SpareParts.DeterminePartOfDay();
+            WebRequest weatherRequest = WebRequest.Create(@"http://api.openweathermap.org/data/2.5/weather?zip=94040,us&appid=8b3de9d1132f5c7199d2650d858cef68");
+            weatherRequest.ContentType = "application/json; charset=utf-8";
+
+            WeatherResponse jsonResponse = new WeatherResponse();
+
+            using (HttpWebResponse weatherResponse = weatherRequest.GetResponse() as HttpWebResponse)
+            {
+                if (weatherResponse.StatusCode != HttpStatusCode.OK)
+                    throw new Exception(String.Format(
+                    "Server error (HTTP {0}: {1}).",
+                    weatherResponse.StatusCode,
+                    weatherResponse.StatusDescription));
+                DataContractJsonSerializer jsonSerializer = new DataContractJsonSerializer(typeof(WeatherResponse));
+                object objResponse = jsonSerializer.ReadObject(weatherResponse.GetResponseStream());
+                jsonResponse
+                = objResponse as WeatherResponse;
+                
+            }
+
+
+            ViewBag.CurrentMonth = SpareParts.GetCurrentMonth(DateTime.Now.Month);
+            ViewBag.CurrentDayOfMonth = DateTime.Now.Day;
+            ViewBag.CurrentDayOfWeek = DateTime.Now.DayOfWeek;
+            ViewBag.CurrentHour = DateTime.Now.Hour;
+            ViewBag.CurrentMinute = DateTime.Now.Minute;
+            ViewBag.CurrentSecond = DateTime.Now.Second;
+            ViewBag.CurrentTemp = (jsonResponse.main.Temp - 273.15) * 1.8 + 32;
+            ViewBag.WeatherDescription = jsonResponse.weather[0].Description;
+            
+
+
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            
 
             return View();
         }
---
+++ RHAA/Models/WeatherResponse.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace RHAA.Models
+{
+    [DataContract]
+    public class WeatherResponse
+    {
+        [DataMember(Name = "coord")]
+        public Coord coord { get; set; }
+        [DataMember(Name ="weather")]
+        public Weather[] weather { get; set; }
+        [DataMember(Name = "main")]
+        public Main main { get; set; }
+    }
+
+
+    [DataContract]
+    public class Coord
+    {
+        [DataMember(Name = "lon")]
+        public string Lon { get; set; }
+        [DataMember(Name = "lat")]
+        public string Lat { get; set; }
+    }
+
+    [DataContract]
+    public class Weather
+    {
+        [DataMember(Name = "id")]
+        public string Id { get; set; }
+        [DataMember(Name = "main")]
+        public string Main { get; set; }
+        [DataMember(Name = "description")]
+        public string Description { get; set; }
+    }
+
+    [DataContract]
+    public class Main
+    {
+        [DataMember(Name = "temp")]
+        public double Temp { get; set; }
+    }
+
+
+}
